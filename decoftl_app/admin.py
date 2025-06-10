dbe9b2498c9ea687f05719f3d15d665b97aeebbf
Pablo-wood <wisebuh3@gmail.com>
2025-06-10 19:23:47 +0100

    Integrated database and route

--- decoftl_app/admin.py
+++ decoftl_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Admin, Rider
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Rider)
+# This code registers the Admin and Rider models with the Django admin site, allowing them to be managed through the admin interface.
--- decoftl_app/models.py
+++ decoftl_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.username} {self.email} {self.phone_number} {self.password}'
+    
+class Rider(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    address = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f'{self.username} {self.phone_number} {self.email} {self.address} {self.age}'
