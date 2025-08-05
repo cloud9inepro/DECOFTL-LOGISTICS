5d2361fcbf776dce248806f1f1644fa8887dfc8d
cloud9inepro <giftemmanuel84@gmail.com>
2025-08-05 10:42:08 +0100

    fiexed bugs

--- DECOFTL/src/main.tsx
+++ DECOFTL/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Contact from './pages/Contact';
+
 
 
 
